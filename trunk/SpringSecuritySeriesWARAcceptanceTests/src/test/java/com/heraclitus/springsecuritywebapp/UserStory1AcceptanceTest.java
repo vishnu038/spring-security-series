b79d37b1b1ce9e4207e4640cdb882b790f9edff8
heraclitusonsoftware <heraclitusonsoftware@2fd2a112-c1b8-11dd-90dc-1bd47c2f18c4>
2008-12-04 07:20:00 +0000

    Part2: add acceptance test for user story 1. Implement enough through spring mvc and spring security to get it passing.

---
+++ trunk/SpringSecuritySeriesWARAcceptanceTests/src/test/java/com/heraclitus/springsecuritywebapp/UserStory1AcceptanceTest.java
@@ -0,0 +1,37 @@
+package com.heraclitus.springsecuritywebapp;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+
+import org.junit.After;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * I contain end-to-end acceptance/functional tests for the simple web
+ * application with spring security.
+ * 
+ * I specifically contain tests that verify the behavior of UserStory1.
+ */
+public class UserStory1AcceptanceTest {
+    
+    private final WebDriver driver = new FirefoxDriver();
+    
+    @After
+    public void aTearDownMethodForEachTest() {
+        // close firefox browser that was launched after each test
+        driver.close();
+    }
+    
+    @Test
+    public void shouldBeAskedToAuthenticateWhenTryingToVisitASecurePageAndNotLoggedIn() {
+        
+        driver.get("http://localhost:8080/springsecuritywebapp/home.htm");
+        
+        assertThat(driver.getTitle(),
+                is(not("Home: Spring Security Web Application")));
+        assertThat(driver.getTitle(), is("Login Page"));
+    }
+}
