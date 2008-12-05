69ef0273e1ace3de0e686e548f5141e06c8749c3
heraclitusonsoftware <heraclitusonsoftware@2fd2a112-c1b8-11dd-90dc-1bd47c2f18c4>
2008-12-05 01:42:28 +0000

    tag state of code after part 4

---
+++ tags/SpringSecuritySeriesWAR-Part4/SpringSecuritySeriesWAR/web/WEB-INF/jsp/admin.jsp
@@ -0,0 +1,17 @@
+<%@ page session="true"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<html>
+
+<head>
+<title>Admin: Spring Security Web Application</title>
+
+</head>
+
+<body>
+
+Admin page: only administrative users should see this page.
+
+</body>
+
+</html>
---
+++ tags/SpringSecuritySeriesWAR-Part4/SpringSecuritySeriesWARAcceptanceTests/src/test/java/com/heraclitus/springsecuritywebapp/UserStory5AcceptanceTest.java
@@ -0,0 +1,133 @@
+package com.heraclitus.springsecuritywebapp;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.containsString;
+
+import org.junit.After;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * I contain end-to-end acceptance/functional tests for the simple web
+ * application with spring security.
+ * 
+ * I specifically contain tests that verify the behaviour of UserStory 4.
+ */
+public class UserStory5AcceptanceTest {
+    
+    private final WebDriver driver = new FirefoxDriver();
+    
+    @After
+    public void aTearDownMethodForEachTest() {
+        // close firefox browser that was launched after each test
+        driver.close();
+    }
+    
+    @Test
+    public void shouldForwardUserToPageTheyAttemptedToReachPreviouslyAfterSuccessfulLogin() {
+        
+        // try to access admin
+        driver.get("http://localhost:8080/springsecuritywebapp/admin.htm");
+        
+        // login on login page
+        final WebElement usernameField = driver.findElement(By
+                .name("j_username"));
+        usernameField.sendKeys("username");
+        
+        final WebElement passwordField = driver.findElement(By
+                .name("j_password"));
+        passwordField.sendKeys("password");
+        
+        passwordField.submit();
+        
+        // verify we are not at admin and not (home)
+        assertThat(driver.getTitle(),
+                is(not("Home: Spring Security Web Application")));
+        assertThat(driver.getTitle(),
+                is("Admin: Spring Security Web Application"));
+        
+    }
+    
+    @Test
+    public void shouldRemainOnLoginPageWithInformativeMessageWhenAuthenticationFailsDueToIncorrectPassword() {
+        
+        // try to get to home.htm
+        driver.get("http://localhost:8080/springsecuritywebapp/login.jsp");
+        
+        final WebElement usernameField = driver.findElement(By
+                .name("j_username"));
+        usernameField.sendKeys("username_does_not_exist");
+        
+        final WebElement passwordField = driver.findElement(By
+                .name("j_password"));
+        passwordField.sendKeys("password");
+        
+        passwordField.submit();
+        
+        // state verification
+        assertThat(driver.getTitle(),
+                is("Login: Spring Security Web Application"));
+        
+        final WebElement informationMessageSection = driver.findElement(By
+                .id("infomessage"));
+        assertThat(informationMessageSection.getText(),
+                containsString("Login failed due to: Bad credentials."));
+    }
+    
+    @Test
+    public void shouldRemainOnLoginPageWithInformativeMessageWhenAuthenticationFailsDueToIncorrectUsername() {
+        
+        // try to get to home.htm
+        driver.get("http://localhost:8080/springsecuritywebapp/login.jsp");
+        
+        final WebElement usernameField = driver.findElement(By
+                .name("j_username"));
+        usernameField.sendKeys("username_does_not_exist");
+        
+        final WebElement passwordField = driver.findElement(By
+                .name("j_password"));
+        passwordField.sendKeys("password");
+        
+        passwordField.submit();
+        
+        // state verification
+        assertThat(driver.getTitle(),
+                is("Login: Spring Security Web Application"));
+        
+        final WebElement informationMessageSection = driver.findElement(By
+                .id("infomessage"));
+        assertThat(informationMessageSection.getText(),
+                containsString("Login failed due to: Bad credentials."));
+    }
+    
+    @Test
+    public void shouldRemainOnLoginPageWithUsernameStillPopulatedWhenAuthenticationFails() {
+        
+        // try to get to home.htm
+        driver.get("http://localhost:8080/springsecuritywebapp/login.jsp");
+        
+        final WebElement usernameField = driver.findElement(By
+                .name("j_username"));
+        usernameField.sendKeys("username_does_not_exist");
+        
+        final WebElement passwordField = driver.findElement(By
+                .name("j_password"));
+        passwordField.sendKeys("password");
+        
+        passwordField.submit();
+        
+        // state verification
+        assertThat(driver.getTitle(),
+                is("Login: Spring Security Web Application"));
+        
+        final WebElement username = driver.findElement(By.name("j_username"));
+        
+        assertThat(username.getValue(), is("username_does_not_exist"));
+    }
+    
+}
